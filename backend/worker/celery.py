9cb3c783d6a14d20c9fbf4643a58e59d2b8e64e3
Ildan <ildan201@yandex.ru>
2025-07-07 16:44:52 +0400

    some changes

---
+++ backend/worker/celery.py
@@ -0,0 +1,13 @@
+import os
+
+from celery import Celery
+
+
+celery = Celery(
+    'worker',
+    broker=f"redis://{os.getenv("REDIS_HOST")}:{os.getenv("REDIS_PORT")}/0",
+    backend=f"redis://{os.getenv("REDIS_HOST")}:{os.getenv("REDIS_PORT")}/1",
+    include=["worker.tasks"]
+)
+
+celery.conf.update(task_track_started=True)
